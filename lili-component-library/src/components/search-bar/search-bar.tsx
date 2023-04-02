2aef1b1ad31439ce5025263797b1765c1992f976
Lilish-Warrior <87679658+Lilish-Warrior@users.noreply.github.com>
2023-04-02 17:58:49 +1000

    Add component logic to filter json

--- lili-component-library/src/components/search-bar/search-bar.tsx
+++ lili-component-library/src/components/search-bar/search-bar.tsx
@@ -7,17 +7,48 @@ import { Component, Host, State, h } from '@stencil/core';
 })
 export class SearchBar {
   @State() input: string;
+  @State() results: any;
 
-  setInput(event: any) {
-    const setInput = event.target.value;
-    console.log(setInput);
+  componentWillLoad() {
+    this.results = [{ name: 'lili' }];
+  }
+
+  setInput(inputValue: any) {
+    this.input = inputValue;
+  }
+
+  setResults(resultArray: []) {
+    this.results = resultArray;
+  }
+
+  fetchData = (value: any) => {
+    fetch('https://jsonplaceholder.typicode.com/users')
+      .then(response => response.json())
+      .then(json => {
+        console.log(json);
+        const results = json.filter(user => {
+          return value && user && user.name && user.name.toLowerCase().includes(value.toLowerCase());
+        });
+        console.log(results);
+        this.setResults(results);
+      });
+  };
+
+  handleChange(inputReceived: any) {
+    const inputValue = inputReceived.target.value;
+    this.setInput(inputValue);
+    this.fetchData(inputValue);
   }
 
   render() {
     return (
       <Host>
         <div class="input-wrapper">
-          <input type="text" placeholder="Type to search..." value={this.input} onChange={e => this.setInput(e)} />
+          <input type="text" placeholder="Type to search..." value={this.input} onChange={e => this.handleChange(e)} />
+          {this.results &&
+            this.results.map(result => {
+              <div>{result.name}</div>;
+            })}
         </div>
       </Host>
     );
